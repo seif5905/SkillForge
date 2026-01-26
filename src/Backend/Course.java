2aeae2b0a8d0d3c82541e584ead1c350df72e307
seif5905 <seifeino2005@gmail.com>
2026-01-26 17:26:45 +0200

    instructor done

--- src/Backend/Course.java
+++ src/Backend/Course.java
@@ -8,16 +8,16 @@ public class Course {
     private String title;
     private String description;
     private String instructorId;
-    private ArrayList<String> Lessons;
-    private ArrayList<String> students;
+    private ArrayList<Lesson> lessons;
+    private ArrayList<Student> students;
 
     public Course(String courseId, String title, String description, String instructorId,
-                  ArrayList<String> Lessons, ArrayList<String> students){
+                  ArrayList<Lesson> lessons, ArrayList<Student> students){
         this.courseId = courseId;
         this.title = title;
         this.description = description;
         this.instructorId = instructorId;
-        this.Lessons = Lessons;
+        this.lessons = lessons;
         this.students = students;
     }
 
@@ -28,9 +28,8 @@ public class Course {
         this.instructorId = instructorId;
     }
 
-    public boolean addLesson(String lessonId, String title, String content){
-        DatabaseManager db = new DatabaseManager();
-
+    public void addLesson(Lesson lesson){
+        this.lessons.add(lesson);
     }
 
     public String getTitle() {
@@ -39,17 +38,17 @@ public class Course {
     public void setTitle(String title) {
         this.title = title;
     }
-    public ArrayList<String> getStudents() {
+    public ArrayList<Student> getStudents() {
         return students;
     }
-    public void setStudents(ArrayList<String> students) {
+    public void setStudents(ArrayList<Student> students) {
         this.students = students;
     }
-    public ArrayList<String> getLessons() {
-        return Lessons;
+    public ArrayList<Lesson> getLessons() {
+        return lessons;
     }
-    public void setLessons(ArrayList<String> lessons) {
-        Lessons = lessons;
+    public void setLessons(ArrayList<Lesson> lessons) {
+        this.lessons = lessons;
     }
     public String getInstructorId() {
         return instructorId;
--- src/Backend/Instructor.java
+++ src/Backend/Instructor.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 
 public class Instructor extends User{
 
-    private ArrayList<String> createdCourses;
+    private ArrayList<Course> createdCourses;
 
     public Instructor(String userid, String role, String username, String email, String passwordHash,
-                   ArrayList<String> createdCourses){
+                   ArrayList<Course> createdCourses){
         super(userid, role, username, email, passwordHash);
         createdCourses = new ArrayList<>();
     }
@@ -21,11 +21,31 @@ public class Instructor extends User{
         }
         Course course = new Course(courseId, title, description, this.getUserid());
         courses.add(course);
-        createdCourses.add(courseId);
+        createdCourses.add(course);
         return true;
     }
 
     public boolean addLesson(String courseId, String lessonId, String title, String content){
+        DatabaseManager db = new DatabaseManager();
+        ArrayList<Course> courses = db.loadCourses();
+        for(Course course : this.createdCourses){
+            if(courseId.equalsIgnoreCase(course.getCourseId())){
+                Lesson lesson = new Lesson(lessonId, title, content);
+                course.addLesson(lesson);
+                return true;
+            }
+        }
+        return false;
+    }
 
+    public ArrayList<Student> viewEnrolledStudents(String courseId){
+        DatabaseManager db = new DatabaseManager();
+        ArrayList<Course> courses = db.loadCourses();
+        for(Course course : this.createdCourses){
+            if(courseId.equalsIgnoreCase(course.getCourseId())){
+                return course.getStudents();
+            }
+        }
+        return new ArrayList<>();
     }
 }
